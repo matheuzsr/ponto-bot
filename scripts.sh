a10f6a96d6c7d1ec17fa1c165017cdc343d2ad9b
matheuzsr <matheusdasilvaroch@gmail.com>
2023-04-04 13:54:50 -0300

    (main) Incluindo validação para criar pasta output

--- scripts.sh
+++ scripts.sh
@@ -5,6 +5,10 @@ cd /home/logcomex/projects/ponto-bot
 
 node dist
 
+if ! [ -d .output/ ]; then
+  mkdir .output && echo "Creating output directory"
+fi
+
 cd .output/
 
 xdg-open "$(ls -1tr | tail -1)"
